ce3961c1c301b96cfb1dd634faf6e8318d621136
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-24 16:10:53 +0400

    add Header

---
+++ src/header/Header.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import MainLayout from "@/main-layout";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  useColorMode,
+} from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Box bg="yellow.500">
+      <MainLayout>
+        <Flex direction="row" alignItems="center" gap="5" mb="10px">
+          <Heading as="h1" size="xl">
+            Восход
+          </Heading>
+        </Flex>
+      </MainLayout>
+    </Box>
+  );
+};
+
+export default Header;
